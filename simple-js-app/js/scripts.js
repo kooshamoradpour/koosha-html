d962363c93c823bbb2d09e5bb735096e3ab10303
Koosha <kushamoradpour@gmail.com>
2023-01-29 12:47:08 -0500

    PokemonList
    Added an empty array
    Added tree pokmon object

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,16 @@
+let pokemonList = [];
+let Charizard= {
+    name: "Charizard",
+    height: 1.7,
+    type: ['fire', 'flying']
+}
+let Pikachu= {
+    name: "Pikachu",
+    height: 0.4,
+    type: ['Electric']
+}
+let Jynx= {
+    name: "Jynx",
+    height: 1.4,
+    type: ['Psychic', 'ice']
+}
